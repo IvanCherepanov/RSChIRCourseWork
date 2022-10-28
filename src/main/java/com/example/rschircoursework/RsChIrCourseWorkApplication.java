54c4100645797a9fa1c5f7a0cad184abf4039393
Ivan <iv.cherepanov@yandex.ru>
2022-10-28 13:40:01 +0300

    Initial commit

---
+++ src/main/java/com/example/rschircoursework/RsChIrCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.rschircoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsChIrCourseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsChIrCourseWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rschircoursework/RsChIrCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rschircoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsChIrCourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
